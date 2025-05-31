32a2c2012ccfa8f35ab756c2e6d121beb41594e6
okapi-21　 <okabekakuji@outlook.jp>
2025-05-31 16:55:54 +0900

    アルゴリズム, result画面の追加

--- app/controllers/diagnoses_controller.rb
+++ app/controllers/diagnoses_controller.rb
@@ -7,10 +7,75 @@ class DiagnosesController < ApplicationController
 
     end
 
-    def result 
-        birthday = params[:birthday]
-        sum = birthday.chars.map(&:to_i).sum
-        type = sum % 12
+    def result
+        # date_params = params[:birthday] は使わず、直接paramsから取得
+        year  = params["[birthday(1i)]"]
+        month = params["[birthday(2i)]"]
+        day   = params["[birthday(3i)]"]
+
+        if year.present? && month.present? && day.present?
+            birthday_str = "#{year}#{month.rjust(2, '0')}#{day.rjust(2, '0')}"
+            sum = birthday_str.chars.map(&:to_i).sum
+            @type = sum % 12 
+            # ここで診断結果を@resultなどにセットしてビューで使う
+            case @type
+            when 0
+                @headline = "あなたはやんちゃパロットタイプ!!"
+                @description = "あなたのエネルギーは...."
+                @image = "party_parrots/parrot_01.png"
+            when 1
+                @headline = "あなたはパイレーツパロットタイプ!!"
+                @description = "いいね"
+                @image = "party_parrots/parrot_02.png"
+            when 2
+                @headline = "あなたは博士パロットタイプ!!"
+                @description = "いいね"
+                @image = "party_parrots/parrot_03.png"
+            when 3
+                @headline = "あなたは天使パロットタイプ!!"
+                @description = "いいね"
+                @image = "party_parrots/parrot_04.png"
+            when 4
+                @headline = "あなたは優等生パロットタイプ!!"
+                @description = "いいね"
+                @image = "party_parrots/parrot_05.png"
+            when 5
+                @headline = "あなたはDJパロットタイプ!!"
+                @description = "いいね"
+                @image = "party_parrots/parrot_06.png"
+            when 6
+                @headline = "あなたはカウボーイパロットタイプ!!"
+                @description = "いいね"
+                @image = "party_parrots/parrot_07.png"
+            when 7
+                @headline = "あなたは魔術師パロットタイプ!!"
+                @description = "いいね"
+                @image = "party_parrots/parrot_07.png"
+            when 8
+                @headline = "あなたは英国紳士パロットタイプ!!"
+                @description = "いいね"
+                @image = "party_parrots/parrot_09.png"
+            when 9
+                @headline = "あなたは百戦錬磨の王者パロットタイプ!!"
+                @description = "いいね"
+                @image = "party_parrots/parrot_10.png"
+            when 10
+                @headline = "あなたはネコ耳パロットタイプ!!"
+                @description = "いいね"
+                @image = "party_parrots/parrot_11.png"
+            when 11
+                @headline = "あなたはスポーツ少年パロットタイプ!!"
+                @description = "いいね"
+                @image = "party_parrots/parrot_12.png"
+            end
+        render :result
+        else
+            flash.now[:danger] = "誕生日を入力してください"
+            render :new , status: :unprocessable_entity
+        end
     end
+
+    private
+    
     
 end
