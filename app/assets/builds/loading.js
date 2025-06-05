a5ea1e350bde8c4ebbdee0ec8cd3dd386b479046
okapi-21　 <okabekakuji@outlook.jp>
2025-06-05 13:02:59 +0900

    loadign画面修正

--- app/assets/builds/loading.js
+++ app/assets/builds/loading.js
@@ -1,18 +1,24 @@
 // app/javascript/loading.js
-document.addEventListener("DOMContentLoaded", function () {
-  const container = document.getElementById("loading-gif-container");
-  let gifs = [];
-  if (container) {
-    gifs = JSON.parse(container.dataset.gifs);
-  }
+document.addEventListener("DOMContentLoaded", function() {
+  const gifs = [
+    "loading_gifs/reversecongaparrot.gif",
+    "loading_gifs/shuffleparrot.gif",
+    "loading_gifs/beltparrot.gif",
+    "loading_gifs/dealwithitnowparrot.gif",
+    "loading_gifs/christmasparrot.gif",
+    "loading_gifs/sleepingparrot.gif",
+    "loading_gifs/mailparrot.gif",
+    "loading_gifs/dabparrot.gif",
+    "loading_gifs/reactparrot.gif"
+  ];
   const loadingGif = document.getElementById("loading-gif");
-  if (loadingGif && gifs.length > 0) {
+  if (loadingGif) {
     const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
     loadingGif.src = randomGif;
   }
   const form = document.querySelector("form");
   if (form) {
-    form.addEventListener("submit", function () {
+    form.addEventListener("submit", function() {
       document.getElementById("loading-overlay").style.display = "block";
     });
   }
