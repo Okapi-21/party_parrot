f295c6052208fdbe4293e535bea4d4570e675bc6
okapi-21　 <okabekakuji@outlook.jp>
2025-06-06 10:45:00 +0900

    fixed_rubocopeerror

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "diagnoses#index"
-  
+
 
   resources :diagnoses, only: %i[index new create]
-  
-  get 'diagnoses/result/:id', to: "diagnoses#result", as: :diagnosis_result
+
+  get "diagnoses/result/:id", to: "diagnoses#result", as: :diagnosis_result
 end
